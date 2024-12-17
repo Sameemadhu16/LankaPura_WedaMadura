2158e40f8577f9871b98ec340a6ca70c69f6c528
LAHITUVISH01 <vishmithahiru68@gmail.com>
2024-12-17 16:18:33 +0530

    Made few changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
-function App() {
-
-  return (
+function App() 
+{
+  return 
+  (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-blue-500">
       <h1 className="text-center text-5xl font-extrabold text-white drop-shadow-lg">
         HELLO LANKAPURA
@@ -10,3 +11,6 @@ function App() {
 }
 
 export default App
+
+
+
