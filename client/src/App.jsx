f26edc99b5f2fb9b11de31c1161b822bd225a33c
LAHITUVISH01 <vishmithahiru68@gmail.com>
2024-12-17 15:58:08 +0530

    Created initial web application setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-blue-500">
+      <h1 className="text-center text-5xl font-extrabold text-white drop-shadow-lg">
+        HELLO LANKAPURA
+      </h1>
+    </div>
+  )
+}
+
+export default App
