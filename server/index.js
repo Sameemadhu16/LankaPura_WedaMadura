6764c42f04418fb5fa7fefaf70dd9a764324422e
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-01-06 19:27:03 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 // import dotenv from 'dotenv';
 
 // dotenv.config();
+
+mongoose
+    .connect(
+        'mongodb+srv://lankapura:lankapura@cluster0.uqmkz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => {
+      console.log('Connected to MongoDB');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
 const app = express();
 app.use(cors());
