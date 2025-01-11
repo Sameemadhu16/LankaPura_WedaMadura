20af1cb9c3cf2643c6e716029404abb3c20e11f8
P.D.S.MADHURANGA <2022cs113@stu.ucsc.cmb.ac.lk>
2025-01-11 23:15:27 +0530

    Fix client side error and database changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
-function App() 
-{
-  return 
-  (
+function App() {
+  return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-green-400 to-blue-500">
       <h1 className="text-center text-5xl font-extrabold text-white drop-shadow-lg">
         HELLO LANKAPURA
       </h1>
     </div>
-  )
+  );
 }
 
-export default App
-
-
-
+export default App;
