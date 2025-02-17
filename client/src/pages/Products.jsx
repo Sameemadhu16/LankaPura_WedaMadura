b183afb6ee13830a749876b0d4f72a81405fdfb3
VISHMITHAWDL <vishmithahiru68@gmail.com>
2025-02-17 12:11:34 +0530

    Changes updated

--- client/src/pages/Products.jsx
+++ client/src/pages/Products.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import landingx from '../assets/Home_Assets/landing_banner.jpg';
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
+  const tap = {
+    backgroundImage: `url(${landingx})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '800px',
+    width: '100%',
+  };
 
-export default Products
+  return (
+    <div>
+      <div style={tap} className="relative flex items-center justify-center">
+        <div className="absolute top-3/4 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white text-7xl font-[playfair] font-semibold px-4 py-2">
+          OUR PRODUCTS
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Products;
